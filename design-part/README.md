129bf600efd80216d5c37d0d6616d403ebcc4727
Sajid Sajad <sajidsajadkhan416@gmail.com>
2026-04-25 13:35:35 +0530

    install laravel app

---
+++ design-part/README.md
@@ -0,0 +1,20 @@
+# Laravel Workshop
+
+## Local setup
+
+This is a minimal Vite app.
+
+```sh
+Use Node.js v22 or later, as this project relies on features required by Vite and modern tooling.
+```
+To run it locally, first install the dependencies:
+
+```sh
+npm install
+```
+
+Then, start the dev server:
+
+```sh
+npm run dev
+```
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss()],
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
